f0478eddf50a12c2e59df1e302f95498cc370c2f
Matthew Brien <MatthewBrien@outlook.com>
2017-07-27 15:58:53 -0400

    added -source argument. accepts a git url, a directory, or defaults to current directory

--- Packesel.pl
+++ Packesel.pl
@@ -2,8 +2,29 @@
 
 use strict;
 use warnings;
-
+use Getopt::Long qw(GetOptions);
 require "list.pl";
+
+#command line default arguments
+my $source = "./";
+
+GetOptions(
+'source=s' => \$source,
+) or die "Usage: $0 --source Dir or Git Repo";
+
+sub is_git_url{
+  if($_[0] =~ /http.*\.git/)
+  {  return 1;}
+  else
+  {  return 0;}
+}
+
+sub is_valid_path{
+  if(system "ls $_[0] > /dev/null 2>&1")
+  {  return 0;}
+  else
+  {  return 1;}
+}
 
 sub is_git_repo{
   #if param passed, check if that directory is a git repo
@@ -19,19 +40,12 @@ sub is_git_repo{
     return 1;
 }
 
-if(@ARGV){
-  if(is_git_repo($ARGV[0])){
-    print "$ARGV[0] is a git repo\n";
-  }
-  else{
-    print "$ARGV[0] is not a git repo\n";
-  }
+if(is_git_url($source)){
+ say ("Get around to writing code to package a git url");
+}
+elsif(is_valid_path($source)){
+  say ("Get around to wring code to package this directory");
 }
 else{
-  if(is_git_repo()){
-    print "current directory is git repo\n";
-  }
-  else{
-    print "current directory is not git repo\n";
-  }
+  say( "package this directory I guess");
 }
